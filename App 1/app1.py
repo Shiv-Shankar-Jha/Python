4f32a558f11441ce1a4befc51e5dc7534b18755b
Shiv Shankar Jha <nsajha932@gmail.com>
2026-01-03 19:37:05 +0530

    Implemented Complete and exit buttons

--- App 1/app1.py
+++ App 1/app1.py
@@ -3,27 +3,42 @@ import FreeSimpleGUI as sg
 
 
 label = sg.Text("Type in a to-do")
+
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
+
 add_button = sg.Button("Add")
+
 list_box = sg.Listbox(values=functions.get_todos(), key='todos', 
                       enable_events=True, size=[45, 10])
+
 edit_button = sg.Button("Edit")
 
+complete_button = sg.Button("Complete")
+
+exit_button = sg.Button("Exit")
+
 window = sg.Window('My To-Do App', 
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
-                   font=('Helvetica', 15))
+                   layout=[[label], 
+                           [input_box, add_button], 
+                           [list_box, edit_button],[complete_button],
+                           [exit_button]],
+                   font=('Helvetica', 25))
 
 while True:
     event, values = window.read()
     print(1, event)
     print(2, values)
     print(3, values['todos'])
+
     match event:
+
         case "Add":
             todos = functions.get_todos()
             new_todo = values["todo"] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
         case "Edit":
             try:
                 todo_to_edit = values['todos'][0]
@@ -36,9 +51,23 @@ while True:
                 functions.write_todos(todos)
 
                 window['todos'].update(values=todos)
+
             except IndexError:
                 sg.popup("Please select an item first", font=('Helvetica', 15))
-        
+
+
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+
+
+        case "Exit":
+            break
+
         case "todos":
             window['todo'].update(value=values['todos'][0])
         
