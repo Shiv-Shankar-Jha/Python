7653f1b20e0292add7ddbfd573f654620def37c4
Shiv Shankar Jha <nsajha932@gmail.com>
2026-02-03 19:28:10 +0530

    some small changes to gui

--- App 1/WebGUI.py
+++ App 1/WebGUI.py
@@ -1,3 +1,13 @@
 import streamlit as st
+import functions
 
-st.title("Welcome to the Web GUI Application")
+todos = functions.get_todos()
+
+st.title("Todo App")
+st.subheader("Monetize your tasks.")
+st.write("This will help you organize and prioritize your tasks effectively.")
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="Add a new todo:", placeholder="Enter todo here...")
