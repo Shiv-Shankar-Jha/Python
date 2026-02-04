71f721c3938d82aeb254b0fba775440c977ffb28
Shiv Shankar Jha <nsajha932@gmail.com>
2026-02-04 19:14:48 +0530

    Add Feature Of adding todos

--- App 1/WebGUI.py
+++ App 1/WebGUI.py
@@ -3,6 +3,11 @@ import functions
 
 todos = functions.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
 st.title("Todo App")
 st.subheader("Monetize your tasks.")
 st.write("This will help you organize and prioritize your tasks effectively.")
@@ -10,4 +15,5 @@ st.write("This will help you organize and prioritize your tasks effectively.")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="Add a new todo:", placeholder="Enter todo here...")
+st.text_input(label="Add a new todo:", placeholder="Enter todo here...",
+              on_change=add_todo, key='new_todo')
